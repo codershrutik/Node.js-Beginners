2a029d91d9341856c41ca7e90988c242f725ce4a
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-16 10:51:39 +0000

    Calling a directory from a JS file

---
+++ src/shelter/index.js
@@ -0,0 +1,8 @@
+const blue = require('./blue');
+const sadie = require('./sadie');
+const Janet = require('./Janet');
+
+const allCats = [blue, sadie, Janet];
+console.log(allCats);
+
+module.exports(allCats);
