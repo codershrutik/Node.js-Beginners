efe6c5145969885fe6eda195e86f4c0d805517aa
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-16 11:11:35 +0000

    Using NPM packages

--- jokes/index.js
+++ jokes/index.js
@@ -1,5 +1,6 @@
 const jokes = require('give-me-a-joke');
+const colord = require('colors');
 // console.dir(jokes);
 jokes.getRandomDadJoke(function(junk){
-    console.log(jokes);
+    console.log(jokes.rainbow);
 });
